64ef749d6ee1a60d32e81d94166af956673d9b14
maksimmost <maksim.mostovoj.al@gmail.com>
2025-11-27 14:47:00 +0300

    новые

---
+++ tests/realworld.test.js
@@ -0,0 +1,57 @@
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+
+//todo Нейминг теста
+test("Пользователь может зарегистрироваться используя email и пароль", async ({
+  page,
+}) => {
+  await page.goto("https://realworld.qa.guru/");
+  await page.getByRole("link", { name: "Sign up" }).click();
+  await page.getByRole("textbox", { name: "Your Name" }).click();
+  await page
+    .getByRole("textbox", { name: "Your Name" })
+    .fill("sniper101120251");
+  await page.getByRole("textbox", { name: "Email" }).click();
+  await page
+    .getByRole("textbox", { name: "Email" })
+    .fill("sniper101120251@ya.ru");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page.getByRole("textbox", { name: "Password" }).fill("12345678");
+  await page.getByRole("button", { name: "Sign up" }).click();
+  await expect(page.getByRole("navigation")).toContainText("sniper10112025");
+});
+
+/*
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+// Переменные
+let email = faker.internet.email({provider: 'qa.guru' });
+let name = faker.person.fullName(); // 'Allen Brown'
+let password  = faker.internet.password({ length: 10 });
+
+const url = 'https://realworld.qa.guru/';
+
+const getRegistration = async (page, email, name, password, url) => {
+    await page.goto(url);
+    await page.getByRole('link', { name: 'Sign up' }).click();
+    await page.getByRole('textbox', { name: 'Your Name' }).click();
+    await page.getByRole('textbox', { name: 'Your Name' }).fill(name);
+    await page.getByRole('textbox', { name: 'Email' }).click();
+    await page.getByRole('textbox', { name: 'Email' }).fill(email);
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill(password);
+    await page.getByRole('button', { name: 'Sign up' }).click();
+};
+
+test1('Пользователь может зарегистрироваться используя email и пароль', async ({ page }) => {
+    getRegistration(page, email, name, password, url);  
+    await expect(page.getByRole('navigation')).toContainText(name);
+});
+
+test2('Пользователь может изменить свое имя в профиле', async ({ page }) => {
+    getRegistration(page, email, name, password, url); 
+    // todo Дописать тест на изменение имени в профиле 
+    await expect(page.getByRole('navigation')).toContainText(name);
+    }); 
+    */
